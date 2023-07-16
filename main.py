e9a521f231cd3b548d8dfb3231330344b944fffe
Nikita <groz55612@mail.ru>
2023-07-17 00:26:09 +0300

    1

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import pandas as pd
 import random
 
 "AIzaSyBK0GEKUzpn93pl4DpgADH1HhQxklm1mX0"
+rc = pd.read_csv("data.csv")
+ert = st.write(rc)
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
 title = st.text_input('Movie title', 'Life of Brian')
 st.write('The current movie title is', title)
 df = pd.DataFrame([random.random(), title, 3, 4, 5, 6, 7])
 df.to_csv("data.csv")
+rc = pd.read_csv("data.csv")
+st.write(rc)
