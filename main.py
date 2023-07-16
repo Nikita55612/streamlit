63db8bcd831d66c930557cf23a2ae3347d519242
Nikita <groz55612@mail.ru>
2023-07-17 00:10:10 +0300

    1

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
+import random
 
-
+"AIzaSyBK0GEKUzpn93pl4DpgADH1HhQxklm1mX0"
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
+df = pd.DataFrame([random.random(), title, 3, 4, 5, 6, 7])
+df.to_csv("data.csv")
